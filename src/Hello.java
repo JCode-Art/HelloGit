6ad68dd97794d15d0c598f4b4ae35c0ab6e5d26a
JCode-Art <Jclausen7@huskers.unl.edu>
2024-08-30 16:50:27 -0500

    Ok

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * Author: Julia Clausen 
+ * Date: 08/30/2005
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author Julia
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+		System.out.println("Julia Clausen");
+	}
+}
